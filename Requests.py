33574f203baf7380f0b9b553c1909296b1b6687d
Tilen Lucovnik <tilen.lucovnik95@gmail.com>
2016-10-26 12:20:44 +0200

    Dodal orodja

---
+++ Requests.py
@@ -0,0 +1,29 @@
+import re
+import orodja
+
+def pocisti_sahisti(sahisti):
+    infos = sahisti.groupdict()
+    infos['rank'] = int(infos['rank'])
+    infos['ime'] = infos['ime'].strip()
+    infos['drzava'] = infos['drzava'].strip()
+    infos['rating'] = int(infos['rating'])
+    infos['letnik'] = int(infos['letnik'])
+    return infos
+
+def naredi_csv_datoteke():
+    sahist_regex = re.compile(
+        r'<tr bgcolor=#.*?><td width=10>&nbsp;(?P<rank>\d+)</a></td><td>&nbsp;'
+        r'(?P<ime>.*?)</td><td>&nbsp;g</td><td>&nbsp;'
+        r'(?P<drzava>\w{3})</td><td>&nbsp;'
+        r'(?P<rating>\d{4})</td><td>&nbsp;\d*?</td><td>&nbsp;'
+        r'(?P<letnik>\d{4})</td></tr>'
+        , flags=re.DOTALL)
+    sahisti = []
+    for html_datoteka in orodja.datoteke('lista/'):
+        for sahist in re.finditer(sahist_regex, orodja.vsebina_datoteke(html_datoteka)):
+            sahisti.append(pocisti_sahisti(sahist))
+    orodja.zapisi_tabelo(sahisti, ['rank', 'ime', 'drzava', 'rating', 'letnik'], 'sahisti.csv')
+
+
+naredi_csv_datoteke()
+
---
+++ potegni.py
@@ -0,0 +1,10 @@
+import orodja
+import re
+
+def potegni():
+    for stran in range(1, 422, 4):
+        osnovni_naslov = 'https://ratings.fide.com/toparc.phtml'
+        parametri = 'cod='
+        naslov = '{}?{}&page={}'.format(osnovni_naslov, parametri, stran)
+        datoteka = 'lista/{:003}.html'.format(stran)
+        orodja.shrani(naslov, datoteka)
